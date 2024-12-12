0c8610eb62b48e4ee75767282ae308d404037fc4
Enzzo Morais de Olinda <enzzomorais0@gmail.com>
2024-12-12 15:39:39 -0300

    Backend initial setup

---
+++ Backend/Controllers/RestaurantsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DishHub.API.Controllers;
+
+[ApiController]
+[Route("/restaurants")]
+public class RestaurantsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAllRestaurants()
+    {
+        return NotFound();
+    }
+}
